58aac2dd3c5638e2913affd0a6399859e3ffc6ea
Karol Leśniewski <lesiulesniewski@gmail.com>
2023-03-01 18:34:42 +0100

    finish website fo mobile

---
+++ web.js
@@ -0,0 +1,7 @@
+const x_close = addEventListener('.fa-solid');
+const nav__href = addEventListener('.main__href');
+const second__main = addEventListener('.second__main');
+
+function openNav() {
+document.getElementsByClassName('main__href')
+}
